7ca39af628fe284e305d4a0f585f45c1f7f5358a
FateJH <Jason_DiDonato@yahoo.com>
2017-03-07 07:50:47 -0500

    initial work on InvalidTerrainMessage and tests

---
+++ common/src/test/scala/game/InvalidTerrainMessageTest.scala
@@ -0,0 +1,17 @@
+// Copyright (c) 2017 PSForever
+package game
+
+import org.specs2.mutable._
+import net.psforever.packet._
+import net.psforever.packet.game._
+import scodec.bits._
+
+class InvalidTerrainMessageTest extends Specification {
+  "decode" in {
+    ko
+  }
+
+  "encode" in {
+    ko
+  }
+}
