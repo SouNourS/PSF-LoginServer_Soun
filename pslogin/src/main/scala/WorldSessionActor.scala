ea2143b3134fb62fcf24f4ba8a43b59121bd4d02
sounours <sounours@gmail.com>
2019-12-22 17:14:57 +0100

    Cery

--- pslogin/src/main/scala/WorldSessionActor.scala
+++ pslogin/src/main/scala/WorldSessionActor.scala
@@ -1712,8 +1712,8 @@ class WorldSessionActor extends Actor with MDCContextAware {
 //      cluster ! InterstellarCluster.GetWorld("z3")
 //      cluster ! InterstellarCluster.GetWorld("z4")
 //      cluster ! InterstellarCluster.GetWorld("z5")
-//      cluster ! InterstellarCluster.GetWorld("z6")
-      cluster ! InterstellarCluster.GetWorld("z7")
+      cluster ! InterstellarCluster.GetWorld("z6")
+//      cluster ! InterstellarCluster.GetWorld("z7")
 //      cluster ! InterstellarCluster.GetWorld("z8")
 //      cluster ! InterstellarCluster.GetWorld("z9")
 //      cluster ! InterstellarCluster.GetWorld("z10")
@@ -4886,7 +4886,7 @@ class WorldSessionActor extends Actor with MDCContextAware {
       sendResponse(ChatMsg(ChatMessageType.CMT_GMBROADCAST, true, "",
         "  \\#6Welcome to PSForever! Join us on Discord at http://chat.psforever.net", None))
       sendResponse(ChatMsg(ChatMessageType.CMT_GMBROADCAST, true, "",
-        "  \\#6The default zone is set to Esamir if you control a Facility there. Sanc is the backup default. You can also travel to any continent using WarpGates.", None))
+        "  \\#6The default zone is set to Ceryshen if you control a Facility there. Sanc is the backup default. You can also travel to any continent using WarpGates.", None))
       sendResponse(ChatMsg(ChatMessageType.CMT_GMBROADCAST, true, "",
         "  \\#3Local chat (/l)\\#6 can be seen by members of your faction within 25 meters.", None))
       sendResponse(ChatMsg(ChatMessageType.CMT_GMBROADCAST, true, "",
